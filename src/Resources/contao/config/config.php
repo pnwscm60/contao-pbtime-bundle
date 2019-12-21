5a13cb2ca0da9c52366fe8ffa1a125fc85a04b7c
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-12-21 16:54:31 +0100

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Back end modules
+ * Front end modules
+ */
+use Pnwscm60\PbtimeBundle\Module\ModuleErfassen;
+$GLOBALS['FE_MOD']['pbtime'] = [ 
+	'erfassen' => ModuleErfassen::class
+];  
