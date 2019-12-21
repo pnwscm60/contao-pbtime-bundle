60fc3efc03ce0ff7c47a0c3b3a840de7d16b1d99
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2019-12-21 16:43:17 +0100

    Create Pnwscm60PbtimeBundle.php

---
+++ src/Pnwscm60PbtimeBundle.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * This file is part of Pbtime.
+ *
+ * (c) 2019 Markus Schenker
+ *
+ * @license LGPL-3.0-or-later
+ */
+namespace Pnwscm60\PbtimeBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+/* Configure the Contao Pbtime Bundle */
+class Pnwscm60PbtimeBundle extends Bundle
+{
+}
