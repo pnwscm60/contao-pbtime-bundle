ffa63daed73891989eb99cad8db0570adacad182
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-01-24 16:05:48 +0100

    Update plugin.php

--- src/ContaoManager/plugin.php
+++ src/ContaoManager/plugin.php
@@ -12,14 +12,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Pnwscm60\ContaoPbtimeBundle\ContaoPbtimeBundle;
+use Pnwscm60\ContaoPbtimeBundle\Pnwscm60ContaoPbtimeBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(ContaoPbtimeBundle::class)
+            BundleConfig::create(Pnwscm60ContaoPbtimeBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
