4c4fe01e44426000f97dd5d3274eedd70ec436ba
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-01-24 13:53:42 +0100

    Update plugin.php

--- src/ContaoManager/plugin.php
+++ src/ContaoManager/plugin.php
@@ -7,18 +7,19 @@
  * @license LGPL-3.0-or-later
  */
 namespace Pnwscm60\PbtimeBundle\ContaoManager;
+
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create('Pnwscm60\PbtimeBundle\Pnwscm60PbtimeBundle')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
-                ->setReplace(['pbtime']),
+            BundleConfig::create(Pnwscm60PbtimeBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 }
