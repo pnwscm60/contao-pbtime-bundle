4b3e3ee03a45c1bd37952cc35f7eaa5642b5e588
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:12:37 +0100

    Update ContaoPbtimeBundle.php
    add strict

--- src/ContaoPbtimeBundle.php
+++ src/ContaoPbtimeBundle.php
@@ -6,6 +6,8 @@
  *
  * @license LGPL-3.0-or-later
  */
+declare(strict_types=1);
+
 namespace Pnwscm60\ContaoPbtimeBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 /* Configure the Contao Pbtime Bundle */
