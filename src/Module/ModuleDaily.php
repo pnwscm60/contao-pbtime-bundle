4c5642b94e931159cf5b32227c5b78d34b6ae811
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:24:11 +0100

    Update ModuleDaily.php
    add strict

--- src/Module/ModuleDaily.php
+++ src/Module/ModuleDaily.php
@@ -1,4 +1,16 @@
 <?php
+declare(strict_types=1);
+
+/*
+ * This file is part of pbwork.
+ * 
+ * (c) Markus Schenker 2022 <scm@olternativ.ch>
+ * @license LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/pnwscm60/contao-pbwork-bundle
+ */
+
 namespace Pnwscm60\ContaoPbtimeBundle\Module;
 class ModuleDaily extends \Contao\Module
 {
