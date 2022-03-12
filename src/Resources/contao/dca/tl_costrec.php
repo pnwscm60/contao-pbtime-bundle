e1420e123517ecc54cabb79fd2eee67c54a981d0
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:31:05 +0100

    Update tl_costrec.php

--- src/Resources/contao/dca/tl_costrec.php
+++ src/Resources/contao/dca/tl_costrec.php
@@ -1,9 +1,14 @@
 <?php
-/**
-DCA für costrec für pbtime
-© 2016 Markus Schenker, Phi Network
- */
-/**
+declare(strict_types=1);
+
+/*
+ * This file is part of pbwork.
+ * DCA für costrec für pbtime
+ * (c) Markus Schenker 2022 <scm@olternativ.ch>
+ * @license LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/pnwscm60/contao-pbwork-bundle
  * Table tl_costrec
  */
 $GLOBALS['TL_DCA']['tl_costrec'] = array
