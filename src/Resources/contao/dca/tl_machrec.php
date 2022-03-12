e4e1be476ca558859b5c257c6eba3ded83345bc8
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:34:03 +0100

    Update tl_machrec.php

--- src/Resources/contao/dca/tl_machrec.php
+++ src/Resources/contao/dca/tl_machrec.php
@@ -1,8 +1,15 @@
 <?php
+declare(strict_types=1);
+
 /*
-*   DCA für machrec for pbtime
-*   © 2016 Markus Schenker, Phi Network
-* Table tl_machrec for pbtime
+ * This file is part of pbwork.
+ * DCA für machrec for pbtime
+ * (c) Markus Schenker 2022 <scm@olternativ.ch>
+ * @license LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/pnwscm60/contao-pbwork-bundle
+ * Table tl_machrec for pbtime
 */
 $GLOBALS['TL_DCA']['tl_machrec'] = array
 (
