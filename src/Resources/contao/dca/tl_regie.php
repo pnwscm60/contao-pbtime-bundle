5369350f6a92c0f857bff428a82051766f0b1d8e
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:36:40 +0100

    Update tl_regie.php

--- src/Resources/contao/dca/tl_regie.php
+++ src/Resources/contao/dca/tl_regie.php
@@ -1,7 +1,14 @@
 <?php
+declare(strict_types=1);
+
 /*
-* DCA für ProTime: regie for pbtime
-* © 2020 Markus Schenker, Phi Network
+ * This file is part of pbwork.
+ * DCA regie for pbtime
+ * (c) Markus Schenker 2022 <scm@olternativ.ch>
+ * @license LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/pnwscm60/contao-pbwork-bundle
 */
 
 /**
