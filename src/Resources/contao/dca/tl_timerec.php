13b8fc482679cd5b5b3e5078d6caea3f8de3dc86
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2022-03-12 18:37:11 +0100

    Update tl_timerec.php

--- src/Resources/contao/dca/tl_timerec.php
+++ src/Resources/contao/dca/tl_timerec.php
@@ -1,8 +1,13 @@
 <?php
-
+declare(strict_types=1);
 /*
-* DCA für Timerec
-* © 2019 Markus Schenker, Phi Network
+ * This file is part of pbwork.
+ * DCA für Timerec
+ * (c) Markus Schenker 2022 <scm@olternativ.ch>
+ * @license LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/pnwscm60/contao-pbwork-bundle
 * Table tl_timerec
 */
 $GLOBALS['TL_DCA']['tl_timerec'] = array
